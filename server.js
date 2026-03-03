74137e7023a425e0b26b712a434d2f557d21f912
Nur Mohammad <nurmohammad151801@gmail.com>
2026-03-03 16:56:38 +0600

    update changes

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const port = 3001;
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, req.url === '/' ? "index.html" : req.url)
+    console.log(filePath);
+    
+    const extName = String(path.extname(filePath)).toLowerCase()
+    const mimeTypes = {
+        '.html':'text/html',
+        '.css':'text/css',
+        '.png':'image/png',
+    }
+
+    const contentType = mimeTypes[extName] || 'application/octet-stream';
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === "ENOENT") {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.end("404:File Not Found BRooooo");
+            }
+        } else {
+            res.writeHead(200, { 'content-type': contentType })
+            res.end(content, 'utf-8');
+        }
+    });
+});
+server.listen(port, () => {
+  console.log(`Server is listening as port ${port}`);
+});
